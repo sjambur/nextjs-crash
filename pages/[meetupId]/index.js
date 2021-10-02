cc516ce8ec239ac2cb8964dad58da2d81626a006
Sachin Jambur <sachin.jambur@neofinancial.com>
2021-10-01 21:05:27 -0600

    changed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -37,7 +37,7 @@ export const getStaticPaths = async () => {
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
