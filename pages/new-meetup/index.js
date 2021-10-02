0f716e8a44ba9bc78c3ff3a7b67b59a5842e8333
Sachin Jambur <sachin.jambur@neofinancial.com>
2021-10-01 20:53:57 -0600

    first commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetupPage = () => {
+  const router = useRouter();
+
+  const addMeetupHandler = async (enteredMeetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    router.push("/");
+  };
+  return (
+    <>
+      <Head>
+        <title>Add a new meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
+};
+
+export default NewMeetupPage;
